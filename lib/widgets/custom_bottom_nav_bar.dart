b37bc37f4ea35fdf8863130ec1fba286deea9db6
HYOJEONG RYU <flowtothemoon355@gmail.com>
2025-09-03 19:26:14 +0900

    refactor : 코드 주석 추가, 색상 누락 부분 수정. 앱 아이콘 이미지 추가

--- lib/widgets/custom_bottom_nav_bar.dart
+++ lib/widgets/custom_bottom_nav_bar.dart
@@ -19,7 +19,7 @@ class CustomBottomNavBar extends StatelessWidget {
 
     return Container(
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: AppColors.grayscale.s30,
 
 // 상단에만 얇은 구분선을 추가
 
@@ -28,15 +28,14 @@ class CustomBottomNavBar extends StatelessWidget {
         ),
       ),
 
-// ✅ SafeArea 위젯으로 감싸서 시스템 영역을 자동으로 피함 (동작 제대로 되는지 확인 필요)
+// SafeArea 위젯으로 감싸서 시스템 영역을 자동으로 피함 (하단바 영역 피하도록 했는데 대신 하단바 영역 색이 검은색으로 뜨긴 함)
 
       child: SafeArea(
-        top: false, // ✅ 상단은 안전 영역이 필요 없으므로 false로 설정
+        top: false, // 상단은 안전 영역이 필요 없으므로 false
 
         child: Container(
-// ✅ 아이콘과 텍스트가 실제로 차지할 영역의 높이를 지정 (피그마 디자인 상 38이 맞음, 근데 실행하니까 58쪽이 더 맞는 것처럼 보이는듯?)
 
-          height: 58,
+          height: 58, // 아이콘과 텍스트가 실제 차지할 영역의 높이 (피그마상 38인데 실행하니까 58이 적정해 보임)
 
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -59,7 +58,7 @@ class CustomBottomNavBar extends StatelessWidget {
       '홈': 'home',
       '대중교통': 'bus',
       '내주변': 'mappoint',
-      '검색': 'search',
+      '검색': 'map',
       '내정보': 'user',
     };
     final String iconName = labelToIconName[label] ?? 'home';
@@ -79,16 +78,15 @@ class CustomBottomNavBar extends StatelessWidget {
             ),
             const SizedBox(height: 2), // 아이콘과 텍스트 사이 간격
 
-            // ✅ Text 위젯을 SizedBox로 감싸서 너비와 높이 고정
+            // Text 위젯을 SizedBox로 감싸서 너비와 높이 고정
             SizedBox(
-              width: 70, // 피그마에서 확인된 너비
-              height: 12, // 피그마에서 확인된 높이 (폰트 크기와 비슷할 가능성 높음)
+              width: 70,
+              height: 12,
               child: Text(
                 label,
                 textAlign: TextAlign.center, // 텍스트를 중앙 정렬
                 style: AppTextStyles.caption2_2.copyWith(
-                  // caption2_2 스타일 고정
-                  fontSize: 12, // ✅ 피그마 높이가 12이므로 폰트 사이즈도 12로 추정
+                  fontSize: 12, // 피그마상 height = 12이므로 글자 크기도 통일시킴
                   color: isSelected
                       ? AppColors.grayscale.s900
                       : AppColors.grayscale.s500,
